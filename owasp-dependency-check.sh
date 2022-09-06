2fa856fcfca9835c600cde1d3902955c1e401ade
ervishnu <49181248+ervishnu@users.noreply.github.com>
2022-09-06 11:02:23 +0530

    Update owasp-dependency-check.sh

--- owasp-dependency-check.sh
+++ owasp-dependency-check.sh
@@ -1,5 +1,7 @@
+rm -rf $(pwd)/dependency-*
 wget https://github.com/jeremylong/DependencyCheck/releases/download/v7.1.2/dependency-check-7.1.2-release.zip
 unzip dependency-check-7.1.2-release.zip 
 cd dependency-check/
 cd bin/
+cd ../../
 ./dependency-check.sh --scan $(pwd)/src --format "ALL" --out $(pwd)/odc-reports/
