fa1a960244c792d764c88af9310f810d2871cc33
ervishnu <49181248+ervishnu@users.noreply.github.com>
2022-09-06 10:54:50 +0530

    Update owasp-dependency-check.sh

--- owasp-dependency-check.sh
+++ owasp-dependency-check.sh
@@ -1,32 +1,5 @@
-#!/bin/sh
-
-DC_VERSION="latest"
-DC_DIRECTORY=$HOME/OWASP-Dependency-Check
-DC_PROJECT="dependency-check scan: $(pwd)"
-DATA_DIRECTORY="$DC_DIRECTORY/data"
-CACHE_DIRECTORY="$DC_DIRECTORY/data/cache"
-
-if [ ! -d "$DATA_DIRECTORY" ]; then
-    echo "Initially creating persistent directory: $DATA_DIRECTORY"
-    mkdir -p "$DATA_DIRECTORY"
-fi
-if [ ! -d "$CACHE_DIRECTORY" ]; then
-    echo "Initially creating persistent directory: $CACHE_DIRECTORY"
-    mkdir -p "$CACHE_DIRECTORY"
-fi
-
-# Make sure we are using the latest version
-docker pull owasp/dependency-check:$DC_VERSION
-
-docker run --rm \
-    -u root \
-    --volume $(pwd):/src:z \
-    --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data:z \
-    --volume $(pwd)/odc-reports:/report:z \
-    owasp/dependency-check:$DC_VERSION \
-    --scan /src \
-    --format "ALL" \
-    --project "$DC_PROJECT" \
-    --out /report
-    # Use suppression like this: (where /src == $pwd)
-    # --suppression "/src/security/dependency-check-suppression.xml"
+wget https://github.com/jeremylong/DependencyCheck/releases/download/v7.1.2/dependency-check-7.1.2-release.zip
+unzip dependency-check-7.1.2-release.zip 
+cd dependency-check/
+cd bin/
+./dependency-check.sh --scan $(pwd)/src --format "ALL" --out $(pwd)/odc-reports/
